b50aa224a98e230b55ccd9ec6dff203502068225
mutou12 <369584771@qq.com>
2025-03-08 15:19:25 +0800

    删除项目中的所有配置文件和代码

--- main.py
+++
@@ -1,9 +0,0 @@
-from typing import Union
-import uvicorn
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
